d758d5199137df9de32eeaa812595d9c5b258096
Caleb <cdonovi1@binghamton.edu>
2015-04-15 15:53:22 -0400

    Add basic main

--- Page_table.h
+++ Page_table.h
@@ -9,6 +9,11 @@ typedef int VPN;
 class Page_table{
     public:
         Page_table();
+        /*
+         * Should return  1 for fault
+         *                0 for in memory
+         *               -1 for error
+         */
         virtual char reference(PID, VPN) = 0;
         virtual void start(PID, unsigned) = 0;
         virtual void terminate(PID) = 0;
--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
+#include <string>
+#include <exception>
+#include <iostream>
+
 int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "Incorrect usage\n";
+        return 1;
+    }
+
+    unsigned int frames;
+    try {
+        frames = std::stoi(argv[1]);
+    } catch (exception &e) {
+        std::cerr << "Bad arg: " << argv[1] << std::endl << e.what() << std::endl;
+        return 1;
+    }
+    PTSim sim;
+    /* construct page tables */
+    /* call sim.add_pt(p) for each page table p */
+
+    sim.simulate(argv[2]);
+    sim.print();
+
 
 	return 0;
 }
